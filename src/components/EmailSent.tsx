eb9a0accfae7f880dd670b220782d724e4d7f375
ywenant <ywenanta2410@gmail.com>
2025-05-30 20:12:07 +0700

    ravising forget passowr and email sent with new design

--- src/components/EmailSent.tsx
+++ src/components/EmailSent.tsx
@@ -1,22 +1,58 @@
 import { useNavigate } from "react-router-dom";
-import AuthLayout from "./BackgroundLayout";
+import React, { useState } from "react";
+import background from "../assets/backgroundSignin.png";
+import logoLight from "../assets/Logo - Light.png";
+import Loading from "../components/Loading";
 
 const EmailSent = () => {
+  const [isLoading, setIsLoading] = useState(false);
   const navigate = useNavigate();
 
   return (
-    <AuthLayout>
-      <div className="absolute top-20 text-center text-white">
-        <h2 className="text-4xl font-bold mb-5">Check Your Email!</h2>
-        <p className="text-lg">A password reset link has been sent.</p>
-        <button
-          onClick={() => navigate("/forgot-password/reset-password")}
-          className="mt-5 bg-white text-blue-400 py-2 px-6 rounded-lg font-semibold hover:bg-gray-50 transition shadow"
-        >
-          Continue
-        </button>
+    <div className="min-h-screen flex flex-col sm:flex-row overflow-hidden relative">
+      {isLoading && <Loading />}
+
+      {/* Logo */}
+      <div className="absolute top-5 left-4 flex items-center gap-2 z-10">
+        <img src={logoLight} alt="logoLight" className="w-16 h-16" />
+        <h1 className="text-xl text-[#78716C]">Serenity</h1>
       </div>
-    </AuthLayout>
+
+      {/* Form Section */}
+      <div className="w-full sm:w-1/2 bg-[#F2EDE2] flex justify-center items-center py-6 sm:py-10 flex-grow relative">
+        <div className="w-full max-w-[500px] sm:max-w-[450px] md:max-w-[500px] px-6 sm:px-8 py-10 mb-12">
+          <h2 className="text-4xl font-bold mb-5">Check Your Email!</h2>
+          <p className="text-lg">A password reset link has been sent.</p>
+          <button
+            onClick={() => navigate("/forgot-password/reset-password")}
+            className="w-full max-w-[310px] mt-5 bg-[#BACBD8] text-black py-2 px-6 rounded-lg font-semibold hover:bg-[#bad2e5] transition shadow"
+          >
+            Continue
+          </button>
+
+          <p className="text-sm text-black text-center sm:text-left mt-6 sm:absolute sm:bottom-5 sm:left-6">
+            Sudah ingat password Anda?{" "}
+            <a
+              href="/login"
+              className="font-bold text-[#8DAABF] hover:text-white cursor-pointer"
+            >
+              Masuk
+            </a>
+          </p>
+        </div>
+      </div>
+
+      {/* Right side with background image */}
+      <div
+        className="hidden sm:block w-1/2 bg-cover bg-center"
+        style={{
+          backgroundImage: `url(${background})`,
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+          minHeight: "100vh",
+        }}
+      />
+    </div>
   );
 };
 
--- src/components/ForgotPasswordForm.tsx
+++ src/components/ForgotPasswordForm.tsx
@@ -1,13 +1,17 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import AuthLayout from "./BackgroundLayout";
 import { ForgotPasswordController } from "../controllers/ForgotPasswordController";
 import type { ResetPasswordErrors } from "../models/ForgotPasswordModel";
+import background from "../assets/backgroundSignin.png";
+import logoLight from "../assets/Logo - Light.png";
+import Loading from "../components/Loading";
+import { InputWithLabel } from "./InputWithLabel";
 
 const ForgotPasswordForm = () => {
   const [email, setEmail] = useState("");
   const [errors, setErrors] = useState<ResetPasswordErrors>({});
   const [isSubmitting, setIsSubmitting] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
   const navigate = useNavigate();
   const controller = new ForgotPasswordController();
 
@@ -27,34 +31,69 @@ const ForgotPasswordForm = () => {
   };
 
   return (
-    <AuthLayout>
-      <div className="absolute top-20">
-        <h2 className="text-4xl font-bold text-white mb-5 text-center">
-          Reset Your Password
-        </h2>
-        <form onSubmit={handleSubmit}>
-          <label className="block text-white text-lg mb-2">Email</label>
-          <input
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            placeholder="youremail@gmail.com"
-            className="w-full p-2 rounded-md bg-white text-gray-900"
-          />
-          {errors.email && (
-            <p className="text-red-500 text-sm mt-1">{errors.email}</p>
-          )}
+    <div className="min-h-screen flex flex-col sm:flex-row overflow-hidden relative">
+      {isLoading && <Loading />}
 
-          <button
-            type="submit"
-            className="w-full bg-white text-blue-400 py-3 rounded-lg font-semibold hover:bg-gray-50 transition shadow disabled:opacity-50 disabled:cursor-not-allowed mt-4"
-            disabled={isSubmitting}
-          >
-            {isSubmitting ? "Sending..." : "Send Reset Email"}
-          </button>
-        </form>
+      {/* Logo */}
+      <div className="absolute top-5 left-4 flex items-center gap-2 z-10">
+        <img src={logoLight} alt="logoLight" className="w-16 h-16" />
+        <h1 className="text-xl text-[#78716C]">Serenity</h1>
       </div>
-    </AuthLayout>
+
+      {/* Form Section */}
+      <div className="w-full sm:w-1/2 bg-[#F2EDE2] flex justify-center items-center py-6 sm:py-10 flex-grow relative">
+        <div className="w-full max-w-[500px] sm:max-w-[450px] md:max-w-[500px] px-6 sm:px-8 py-10 mb-12">
+          <h2 className="text-3xl sm:text-4xl font-light text-[#161F36] text-left">
+            Lupa Kata Sandi?
+            <p className="font-semibold mt-2 text-2xl sm:text-4xl">
+              Reset Password Anda
+            </p>
+          </h2>
+
+          <form onSubmit={handleSubmit} className="space-y-3 mt-6">
+            <InputWithLabel
+              label="Email"
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="Masukkan email Anda"
+            />
+            {errors.email && (
+              <p className="text-red-500 text-sm">{errors.email}</p>
+            )}
+
+            <button
+              type="submit"
+              className="w-full max-w-[385px] bg-[#BACBD8] text-black py-3 rounded-md font-semibold hover:bg-[#bad2e5] transition disabled:opacity-50 disabled:cursor-not-allowed"
+              disabled={isSubmitting}
+            >
+              {isSubmitting ? "Sending..." : "Send Reset Email"}
+            </button>
+          </form>
+
+          <p className="text-sm text-black text-center sm:text-left mt-6 sm:absolute sm:bottom-5 sm:left-6">
+            Sudah ingat password Anda?{" "}
+            <a
+              href="/login"
+              className="font-bold text-[#8DAABF] hover:text-white cursor-pointer"
+            >
+              Masuk
+            </a>
+          </p>
+        </div>
+      </div>
+
+      {/* Right side with background image */}
+      <div
+        className="hidden sm:block w-1/2 bg-cover bg-center"
+        style={{
+          backgroundImage: `url(${background})`,
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+          minHeight: "100vh",
+        }}
+      />
+    </div>
   );
 };
 
